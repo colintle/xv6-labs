5d114f66534369da84daff3f80af6956d9f5f886
Colin Le <colintranle123@gmail.com>
2026-05-26 22:36:29 -0700

    Will need to investigate

--- user/attack.c
+++ user/attack.c
@@ -7,13 +7,16 @@ int
 main(int argc, char *argv[])
 {
   char *mem = sbrk(32 * 4096);
-
+  // the source string offset is 67840
+  // the copied string offset is 69648
+  // will need to investigate why this is
   for(int i = 0; i < 32 * 4096 - 16; i++){
     if(mem[i] == 'T' &&
        mem[i + 1] == 'h' &&
        mem[i + 2] == 'i' &&
        mem[i + 3] == 's'){
 
+      printf("%d\n", i);
       char *secret = mem + i + 16; 
 
       while(*secret != '\0'){
